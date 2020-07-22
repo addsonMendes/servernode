958df87ad40a990d47294d60b971661319e54def
addsonMendes <addson.mendes@hotmail.com>
2020-07-22 00:12:45 -0300

    Corrige arquivo de instalação de dependências

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-npm i nodemon
-npm i express
-npm i body-parser
+npm i -g nodemon
+npm i -g express
+npm i -g body-parser
