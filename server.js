ec421fe3c903fff5b0a5eadd8e9efd51cf1896df
addsonMendes <addson.mendes@hotmail.com>
2020-07-21 23:27:25 -0300

    nova correção arquivo server

--- server.js
+++ server.js
@@ -2,7 +2,5 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-    console.log('Servidor iniciado na porta 56000');
-//   res.write('Hello World!'); //write a response to the client
   res.end('Servidor iniciado...'); //end the response
 }).listen(56000); //the server object listens on port 8080
