5ab0d04be167f12f010cc56a75574f4e42595590
addsonMendes <addson.mendes@hotmail.com>
2020-07-24 12:38:58 -0300

    Correção de inicialização da aplicação

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #install node
 sudo apt-get install curl -y
 # curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install nodejs -y
 # sudo apt-get install -y build-essential
 sudo apt-get install npm -y
