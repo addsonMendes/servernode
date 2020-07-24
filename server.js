e71c521e39ea67acd1d47b12e91f099460c15dc0
addsonMendes <addson.mendes@hotmail.com>
2020-07-24 14:12:28 -0300

    Correção de inicialização da aplicação

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
     res.json({"message":"Server Iniciado"});
 });
 
-var server = app.listen(3000, "localhost", function(){
-    console.log('Escutando 3000');
+var server = app.listen(3333, function(){
+    console.log('Escutando 3333');
 });
