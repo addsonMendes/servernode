a34e6fc097e25f2b4f731277cc4e791c845865ab
addsonMendes <addson.mendes@hotmail.com>
2020-07-24 13:30:19 -0300

    Correção de inicialização da aplicação

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@ sudo apt-get install curl -y
 # curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install nodejs -y
-# sudo apt-get install -y build-essential
+sudo apt-get install -y build-essential
 sudo apt-get install npm -y
 
 #install pm2
