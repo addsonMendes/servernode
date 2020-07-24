c4587e6c179ae943db442e461a09965b8c3ce280
addsonMendes <addson.mendes@hotmail.com>
2020-07-24 12:30:06 -0300

    Correção de inicialização da aplicação

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,5 +5,5 @@ sudo pm2 stop server.js
 # actually start the server
 # sudo pm2 start /var/www/app/server.js -i 0 --name "server"
 
-sudo pm2 start server.js
-# sudo -H -u ubuntu bash -c 'pm2 start /var/www/app/server.js'
+# sudo pm2 start server.js
+sudo -H -u ubuntu bash -c 'pm2 start /var/www/app/server.js'
